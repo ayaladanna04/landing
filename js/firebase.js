f069926372a5c58cf13d9d1e95cd3e08565c4d1f
ayaladanna04 <danaayal@espol.edu.ec>
2025-10-29 22:08:44 -0500

    Firebase

---
+++ js/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/x.y.z/firebase-app.js";
+import { getDatabase, ref, set, push, get, child } from "https://www.gstatic.com/firebasejs/x.y.z/firebase-database.js";
+
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  AUTH_DOMAIN: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  PROJECT_ID: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  STORAGE_BUCKET: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  MESSAGING_SENDER_ID: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  APP_ID: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+const firebaseConfig = { ... };
+
+const app = initializeApp(firebaseConfig);
